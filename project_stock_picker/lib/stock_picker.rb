9b392e9944103130efc978b89f801823f1065e73
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-04 09:17:26 -0400

    feat: completed stock picker

--- project_stock_picker/lib/stock_picker.rb
+++ project_stock_picker/lib/stock_picker.rb
@@ -1,2 +1,25 @@
 class StockPicker
+  # @param [Array<Integer>] prices Array of stock prices.
+  def find_max_profit_days(prices)
+    max_profit = [0, 0]
+    buy_day = 0
+    sell_day = 1
+
+    while sell_day < prices.length
+      if prices[buy_day] < prices[sell_day]
+        max_profit = update_max_profit(prices, max_profit, buy_day, sell_day)
+      else
+        buy_day = sell_day
+      end
+      sell_day += 1
+    end
+    max_profit
+  end
+
+  def update_max_profit(prices, max_profit, buy_day, sell_day)
+    curr_profit = prices[sell_day] - prices[buy_day]
+    curr_max_profit = prices[max_profit[1]] - prices[max_profit[0]]
+    max_profit = [buy_day, sell_day] if curr_max_profit < curr_profit
+    max_profit
+  end
 end
--- project_stock_picker/spec/stock_picker_spec.rb
+++ project_stock_picker/spec/stock_picker_spec.rb
@@ -1,4 +1,16 @@
-require "./lib/stock_picker"
+require './lib/stock_picker'
 
 describe StockPicker do
+  it 'finds the days for maximum profit' do
+    stock_picker = StockPicker.new
+    got = stock_picker.find_max_profit_days([17, 3, 6, 9, 15, 8, 6, 1, 10])
+    expected = [1, 4]
+    expect(got).to eql(expected)
+  end
 end
+
+=begin
+- have to use two pointers
+- both at the start
+- low, high trackers
+=end
